a344a05103ed3ef8668dc2e99d22e521d8c480f4
Ammad Khan <Ammad Khan@Haier-PC.mshome.net>
2020-01-03 16:48:23 +0500

    test cases updated

--- src/test/java/calculator/TestCalculator.java
+++ src/test/java/calculator/TestCalculator.java
@@ -17,23 +17,54 @@ public class TestCalculator {
 	@Test
 	public void testfindMax() {
 		Calculator cal=new Calculator();
-		assertEquals(9,cal.findMax(7, 5, 9));
+		assertEquals(-5,cal.findMax(-7, -5, -9));
 			}
 	@Test
 	public void test2findMax() {
 		Calculator cal=new Calculator();
 		assertEquals(9,cal.findMax(6, 5, 9));
 			}
+
+	@Test
+	public void test3findMax() {
+		Calculator cal=new Calculator();
+		assertEquals(9,cal.findMax(-6, -5, 9));
+			}
+	
 	@Test
 	public void testsquare() {
 		Calculator cal=new Calculator();
-		assertEquals(49,cal.square(7));
+		assertEquals(49,cal.square(-7));
 	}
 	
 	@Test
 	public void test2square() {
 		Calculator cal=new Calculator();
 		assertEquals(9,cal.square(3));
+	}
+	
+	@Test
+	public void test3square() {
+		Calculator cal=new Calculator();
+		assertEquals(0,cal.square(0));
+	}
+	
+	@Test
+	public void testcube() {
+		Calculator cal=new Calculator();
+		assertEquals(0,cal.cube(0));
+	}
+	
+	@Test
+	public void test2cube() {
+		Calculator cal=new Calculator();
+		assertEquals(8,cal.cube(2));
+	}
+	
+	@Test
+	public void test3cube() {
+		Calculator cal=new Calculator();
+		assertEquals(-27,cal.cube(-3));
 	}
 	
 	@AfterClass
